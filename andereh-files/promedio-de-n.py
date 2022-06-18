b1815beae39d19df8c7866b732292639c1538e38
Andereh <ronaldherpc@gmail.com>
2022-06-18 10:40:54 -0400

    Programa para promediar n numeros

---
+++ andereh-files/promedio-de-n.py
@@ -0,0 +1,13 @@
+suma = 0
+
+n_elementos = int(input("Cuantos elementos quiere evaluar?: "))
+
+while n_elementos < 1:
+    n_elementos = int(input("Debe ser un numero positivo, intenta otro: "))
+
+for i in range(1, n_elementos+1):
+    suma += int(input(f"Elemento {i}/{n_elementos}: "))
+
+total = suma/n_elementos
+
+print(f"El promedio es de {total}")
