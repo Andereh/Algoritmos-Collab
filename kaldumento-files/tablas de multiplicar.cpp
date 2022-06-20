4acad6eb393a5e1284a33bfc03b7cfc664c46067
Kaldumento <107769347+Kaldumento@users.noreply.github.com>
2022-06-19 22:11:23 -0400

    Update tablas de multiplicar.cpp

--- kaldumento-files/tablas de multiplicar.cpp
+++ kaldumento-files/tablas de multiplicar.cpp
@@ -4,23 +4,20 @@
  
  int num;
  int main(){
- 	
- 	
-	
- 		
  		cout<<"Hola, estre programa fue hecho para mostrarte las tablas de multiplicar ";
  		cout<<"\n\nIngresa el numero el cual quieres ver su tabla: "; cin>>num;
  		
 	     while (num<1){
 	     cout<<"Lo siento, solo evaluo numeros positivos, intenta otro: "; cin>>num;
-	}
+	                  }
 	     while (num>2000){
 		 cout<<"\nNo te parece un numero muy alto? ingresa otro: "; cin>>num;
-    }
+                             }
  	
  	for(int i=1;i<=10;i++){
  		cout<<num<<" * "<<i<<" = "<<num*i<<endl;
-	 }
+	                     }
  	
  	return 0;
  }
+//LISTO COÑO
