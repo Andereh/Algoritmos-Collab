0aba2077d1d65a38649972fe37d4f4fdc0f9bcbd
Ronald <ronaldherpc@gmail.com>
2022-09-04 12:31:35 -0400

    Primero pedimos las longitudes y luego inicializamos la matriz con esos valores para ahorrar memoria

--- dave5180-files/Matriz_aleatoria.cpp
+++ dave5180-files/Matriz_aleatoria.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <conio.h>
+//#include <conio.h> // no existe para linux
 #include <stdlib.h> // VariableRand = Limite_inferior + rand() % (limite_superior +1 - limite_inferior) ;
-
 #include <time.h>
 
 using namespace std;
@@ -9,11 +8,11 @@ using namespace std;
 int main(){
     srand(time(NULL));
 
-    int ramdom, x, y, matrizAleatoria[100][100];
+    int ramdom, x, y; 
 
     cout<<"Inserte el Numero de filas a generar: "; cin>>x;
     cout<<"Inserte el Numero de columnas a generar: "; cin>>y; cout<<endl;
-
+    int matrizAleatoria[x][y];
     for(int i = 0; i < x; i++){
         for(int j = 0; j < y; j++){
             matrizAleatoria[i][j] = 1 + rand()%(10 - 1);
@@ -23,6 +22,7 @@ int main(){
         cout<<"\n";
     }
 
-    getch();
+    // system("pause");
+    // getch();
     return 0;
 }
