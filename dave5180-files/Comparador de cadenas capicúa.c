1e925e8cd44af027ffa9326e367237ddcfc3e2ba
Dave5180 <107661244+Dave5180@users.noreply.github.com>
2022-09-05 09:15:38 -0400

    litobebe

--- dave5180-files/Comparador de cadenas capicúa.c
+++ dave5180-files/Comparador de cadenas capicúa.c
@@ -8,17 +8,12 @@ printf("Inserte la cadena a comparar: "); gets(cadena);
 	int x = strlen(cadena);
 	char capicua[100];
 	int j = x - 1;
-  
-  //empezamos desde la posicion J que equivale a la longitud de la cadena menos uno; asi nos referirnos a la ultima posicion de la cadena
-  //cada vuelta se le resta uno a J para que se almacene lo que hay en la posicion normal [I] en la nueva cadena por cada vuelta
-  //es decir, se almacena lo que hay en el reverso de la cadena ingresada, en la nueva cadena
-  //si no entendiste, dale laik
-  
-	for (i = 0; i < x; i++){ 
+  	for (i = 0; i < x; i++){
 		capicua[i] = cadena[j];
-		j--;
-	}
-	capicua[x] = '\0';
+			j--;
+		}
+	cadena[x-1] = 0;
+	capicua[x-1] = 0;
   
 	printf("\n\nLa cadena: %s\nSu reverso: %s", cadena, capicua);
 	if (strcmp(cadena, capicua) == 0) printf("\n\nLa cadena es capicua!!");
